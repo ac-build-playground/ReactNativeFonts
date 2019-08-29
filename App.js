96143418f135a7cf3ef9c0cd2db1470cdc3c4502
Evgenii Khramkov <evkhramk@microsoft.com>
2019-08-29 08:56:54 -0700

    disable custom fonts

--- App.js
+++ App.js
@@ -89,7 +89,7 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontFamily: "NotoSans-Bold",
+    // fontFamily: "NotoSans-Bold",
     fontWeight: '600',
     color: Colors.black,
   },
@@ -106,7 +106,6 @@ const styles = StyleSheet.create({
     color: Colors.dark,
     fontSize: 12,
     fontWeight: '600',
-    fontFamily: "NotoSans-Bold",
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
